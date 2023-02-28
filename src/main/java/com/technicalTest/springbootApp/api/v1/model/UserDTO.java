4194346b4daed33e9c34335f267c0f6d9e8fecd8
zioCristia <bicchiericristiano@gmail.com>
2023-02-28 16:56:56 +0100

    Modify UserDto

--- src/main/java/com/technicalTest/springbootApp/api/v1/model/UserDTO.java
+++ src/main/java/com/technicalTest/springbootApp/api/v1/model/UserDTO.java
@@ -1,13 +1,38 @@
 package com.technicalTest.springbootApp.api.v1.model;
 
+import com.technicalTest.springbootApp.api.v1.constraints.BirthDate;
+import com.technicalTest.springbootApp.api.v1.constraints.ResidenceCountry;
+import com.technicalTest.springbootApp.entities.Gender;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class UserDTO {
     private Long id;
+
+    @NotBlank
     private String userName;
+
+    @NotNull(message = "The date of birth is required.")
+    @BirthDate(message = "User must be at least 18 years old")
     private LocalDate birthDate;
+
+    @NotBlank
+    @ResidenceCountry(message = "User must be a France citizen")
     private String residenceCountry;
+
+    @Size(min = 8, max = 14)
+    private String phoneNumber;
+
+    private Gender gender;
 }
