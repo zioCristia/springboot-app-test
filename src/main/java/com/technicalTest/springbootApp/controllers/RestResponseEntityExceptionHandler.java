7a7c6533f026001f66e3a4addfe48c427ef51ff8
zioCristia <bicchiericristiano@gmail.com>
2023-02-28 16:56:42 +0100

    Add exception handling

---
+++ src/main/java/com/technicalTest/springbootApp/controllers/RestResponseEntityExceptionHandler.java
@@ -0,0 +1,48 @@
+package com.technicalTest.springbootApp.controllers;
+
+import com.technicalTest.springbootApp.services.BadRequestException;
+import com.technicalTest.springbootApp.services.ResourceNotFoundException;
+import jakarta.validation.ConstraintViolationException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@ControllerAdvice
+public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler({ResourceNotFoundException.class})
+    public ResponseEntity<Object> handleNotFoundException(Exception exception, WebRequest request){
+
+        return new ResponseEntity<Object>("Resource Not Found", new HttpHeaders(), HttpStatus.NOT_FOUND);
+
+    }
+
+//    @ExceptionHandler({BadRequestException.class})
+//    public ResponseEntity<Object> handleBadRequestException(Exception exception, WebRequest request){
+//
+//        return new ResponseEntity<Object>("Bad request. Make sure all inputs arguments are in the correct format",
+//                new HttpHeaders(), HttpStatus.BAD_REQUEST);
+//
+//    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<?> constraintViolationException(ConstraintViolationException ex, WebRequest request) {
+        List<String> errors = new ArrayList<>();
+
+        ex.getConstraintViolations().forEach(cv -> errors.add(cv.getMessage()));
+
+        Map<String, List<String>> result = new HashMap<>();
+        result.put("errors", errors);
+
+        return new ResponseEntity<>(result, HttpStatus.BAD_REQUEST);
+    }
+}
